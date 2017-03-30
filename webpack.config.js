94dd96dc7ccb917556b08741b198ba4bb305c055
jerry gao <gaojian123@outlook.com>
2017-03-30 17:30:54 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var _p = path.resolve(__dirname,'dist');
+module.exports = {
+	entry:{
+		main:'./app/index.js',
+		vendor:['jquery']
+	},
+	output:{
+		filename:'[name].[chunkHash:5].js',
+		path:path.resolve(__dirname,'dist')
+	},
+	plugins:[
+            new webpack.optimize.CommonsChunkPlugin({
+                names: ['vendor','manifest']
+            }),
+            new HtmlWebpackPlugin({
+            	title:'demo',
+            	template:'index.html'
+            })
+        ]
+}
